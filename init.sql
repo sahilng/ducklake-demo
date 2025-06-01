87fd38847b9bca09be169c03c9bd8209fb3fb892
Sahil Gupta <sahilng1997@gmail.com>
2025-06-01 17:40:54 +0000

    update init.sql

--- init.sql
+++ init.sql
@@ -7,4 +7,3 @@ USE my_ducklake;
 
 ATTACH 'ducklake:postgres:dbname=ducklake_catalog_two host=127.0.0.1 user=admin password=admin' AS my_ducklake_two
     (DATA_PATH 'data_files_two/');
-USE my_ducklake_two;
